2a346aa9dd2b1ca31f7902daf87d6179c50960e3
collinboler <155112701+collinboler@users.noreply.github.com>
2024-05-11 19:12:40 -0400

    Add files via upload
    added 400h

--- Tffrs.java
+++ Tffrs.java
@@ -32,21 +32,23 @@ public class Tffrs {
         String[] fivek = readFile("5000east.txt");
         String[] four = readFile("400east.txt");
         String[] ten = readFile("10000east.txt");
+        String[] fourH = readFile("400heast.txt");
 
         List list1500 = new List(fifteen, "1500");
         List list800 = new List(eight, "800");
-        List list3000s = new List(steeple, "steeple");
+        List list3000s = new List(steeple, "3000s");
         List list5000 = new List(fivek, "5000");
         List list400 = new List(four, "400");
         List list10000 = new List(ten, "10000");
+        List list400h = new List(fourH, "400h");
 
 
         Player player = new Player(input);
-        List[] lists = {list1500, list800, list3000s, list5000, list400, list10000};
+        List[] lists = {list1500, list800, list3000s, list5000, list400, list10000, list400h};
 
         // if event is 5K, don't consider 10K standings
         if (five) {
-            List [] fiveklists = {list1500, list800, list3000s, list5000, list400};
+            List [] fiveklists = {list1500, list800, list3000s, list5000, list400, list400h};
             player.all(fiveklists);
             return player;
         }
